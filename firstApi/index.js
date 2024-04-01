789baec73f434fded268bfba79fe3d610069d486
Adnan Khan <beingadnankhan678@gmail.com>
2024-04-01 12:25:14 +0530

    Params

--- firstApi/index.js
+++ firstApi/index.js
@@ -7,17 +7,22 @@ const app=express();
 // Create router instance for routing form express 
 const router=express.Router();
 // get api creation router.get(path,callback)
-router.get('/hello', function(req,res){
+router.get('/:country/user/:state', function(req,res){
 
     // getting values from query params
-    const name=req.query.product;
-    const brand=req.query.brand;
+    // const name=req.query.product;
+    // const brand=req.query.brand;
 
     // getting query object
-    const data=req.query;
+
+    // const data=req.query;
+    const{name,brand}=req.query;
 
     // console.log(brand)
-    res.send({status :'ok',message:data});
+
+    const countryName=req.params
+    console.log(countryName)
+    res.send({status :'ok',message:{name:name,brand:brand}});
 })
 
 // global middleware for all api calling defined with router
